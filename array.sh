d2936dc2fae181e63a8726a0223874cd6fd0d76f
nari1021 <y.shhh000@gmail.com>
2021-07-21 14:40:26 +0900

    feat: insert explain

--- array.sh
+++ array.sh
@@ -15,8 +15,12 @@ echo =====Array=======
 i=1
 while read line
 do
+    # 첫번째 필드를 읽어와서 index[i]에 저장
     index[i]=$(echo $line | awk '{print $1}')
+    # index 1 : asdf 이런 식으로 출력
     echo index $i : ${index[i]}
+    # i를 1씩 증가시키면서 반복
     let i++
+# txt 파일 끝날때까지 반복
 done < text.txt
 echo =================
